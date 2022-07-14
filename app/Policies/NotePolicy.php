9184408708943f4a61afa6daf3e338a6262a2522
Adam Łatkowski <latek.kalisz@gmail.com>
2022-07-14 14:13:48 +0200

    Repair Dir,Note request, notecontroller and note policy

--- app/Policies/NotePolicy.php
+++ app/Policies/NotePolicy.php
@@ -6,6 +6,7 @@ use App\Models\Dir;
 use App\Models\Note;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Auth;
 
 class NotePolicy
@@ -15,8 +16,8 @@ class NotePolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function viewAny(User $user)
     {
@@ -26,20 +27,23 @@ class NotePolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Note  $note
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Note $note
+     * @return Response|bool
      */
     public function view(User $user, Note $note)
     {
-        return true;
+        $dir = $note->dir;
+        return ((Auth::id() === $user->id) && ( $user->id === $dir->user_id));
     }
+
+
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function create(User $user)
     {
@@ -50,22 +54,23 @@ class NotePolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Note  $note
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Note $note
+     * @return Response|bool
      */
     public function update(User $user, Note $note)
     {
         $dir = $note->dir;
-        return true;
+        return ((Auth::id() === $user->id) && ( $user->id === $dir->user_id));
     }
+
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Note  $note
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Note $note
+     * @return Response|bool
      */
     public function delete(User $user, Note $note)
     {
